76467ebdee6a273b5b31bb09181ae29800f13da5
Allan <a.cassianoweber@gmail.com>
2023-07-21 13:26:07 +0200

    manage posts

--- pages/p/[id].tsx
+++ pages/p/[id].tsx
@@ -1,4 +1,6 @@
 import { GetServerSideProps } from 'next';
+import { useSession } from 'next-auth/react';
+import Router from 'next/router';
 import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import Layout from '../../components/Layout';
@@ -12,16 +14,36 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
     },
     include: {
       author: {
-        select: { name: true },
+        select: { name: true, email: true },
       },
     },
   });
   return {
-    props: post!,
+    props: post || [],
   };
 };
 
+async function publishPost(id: string): Promise<void> {
+  await fetch(`/api/publish/${id}`, {
+    method: 'PUT',
+  });
+  await Router.push('/');
+}
+
+async function deletePost(id: string): Promise<void> {
+  await fetch(`/api/post/${id}`, {
+    method: 'DELETE',
+  });
+  Router.push('/');
+}
+
 const Post: React.FC<PostProps> = (props) => {
+  const { data: session, status } = useSession();
+  if (status === 'loading') {
+    return <div>Authenticating ...</div>;
+  }
+  const userHasValidSession = Boolean(session);
+  const postBelongsToUser = session?.user?.email === props.author?.email;
   let title = props.title;
   if (!props.published) {
     title = `${title} (Draft)`;
@@ -33,10 +55,15 @@ const Post: React.FC<PostProps> = (props) => {
         <h2>{title}</h2>
         <p>By {props?.author?.name || 'Unknown author'}</p>
         <ReactMarkdown children={props.content} />
+        {!props.published && userHasValidSession && postBelongsToUser && (
+          <button onClick={() => publishPost(props.id)}>Publish</button>
+        )}
+        userHasValidSession && postBelongsToUser && (
+        <button onClick={() => deletePost(props.id)}>Delete</button>)
       </div>
       <style jsx>{`
         .page {
-          background: white;
+          background: var(--geist-background);
           padding: 2rem;
         }
 
